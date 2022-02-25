b4ecacfd9165b5b4a11267981026e866d39311c4
Snehil Kumar <1NT20CS179.snehil@nmit.ac.in>
2022-02-25 23:39:35 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,46 @@
+// Beautiful Matrix
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (i = 0; i < n; ++i)
+#define REP(i, k, n) for (i = k; i <= n; ++i)
+#define REPR(i, k, n) for (i = k; i >= n; --i)
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define mii map<int, int>
+#define pqb priority_queue<int>
+#define pqs priority_queue<int, vi, greater<int>>
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define mk(arr, n, type) type *arr = new type[n];
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+
+int main()
+{
+    int arr[5][5];
+    int x, y;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cin >> arr[i][j];
+            if (arr[i][j] == 1)
+            {
+                x = i;
+                y = j;
+            }
+        }
+    }
+    int ans = abs(x - 2) + abs(y - 2);
+    cout << ans << endl;
+    return 0;
+}
